77bf8b813d8022454fc7fe39fc46f42617b57586
yur1ofc <144504409+yur1ofc@users.noreply.github.com>
2026-02-18 22:32:38 -0300

    Add admin route to the application

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Admin from "./pages/Admin"
+import Admin from "./pages/Admin";
 
 const queryClient = new QueryClient();
 
@@ -17,13 +17,13 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/admin" element={<Admin />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
-  <Route path="/admin" element={<Admin />} />
 );
 
 export default App;
